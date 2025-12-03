7c3fc1068c1fd56d2a044cc70fbd181e772b2171
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-12-03 14:49:55 +0600

    Initial Commit

---
+++ HabitAura/HabitAuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitAuraApp.swift
+//  HabitAura
+//
+//  Created by Hafizur Rahman on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitAuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
