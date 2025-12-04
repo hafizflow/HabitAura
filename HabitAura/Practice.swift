6935c0c1af2639caabefba9a932776a976f09b9f
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-12-04 16:09:52 +0600

    Advance Alert & ButtonRepeatBehavior

--- HabitAura/Practice.swift
+++ HabitAura/Practice.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AlertAdvance
 
 struct Practice: View {
     @State private var num = 0
@@ -24,6 +25,55 @@ struct Practice: View {
     }
 }
 
+struct AlertView: View {
+    @State private var showAlert = false
+    @State private var showSheet = false
+    @State private var search = ""
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            Button("Show alert") { showAlert = true }
+                .alert("Title", isPresented: $showAlert) {
+                    Button("OK") {}
+                }
+                .alertContent(isPresented: showAlert) {
+                    VStack {
+                        Text("Custom alert content")
+                        
+                        Rectangle()
+                            .background(.gray.opacity(0.1))
+                            .frame(width: .infinity, height: 40)
+                            .clipShape(.rect(cornerRadius: 16))
+                            .padding(.horizontal)
+                            .overlay {
+                                HStack(spacing: 8) {
+                                    TextField("Hafiz....", text: $search).padding(.horizontal)
+                                    Image(systemName: "magnifyingglass")
+                                        .foregroundStyle(.secondary)
+                                        .padding(.trailing)
+                                }
+                                .padding(.horizontal)
+                            }
+                    }
+                    .padding()
+                }
+            
+            Button("Show confirmation") { showSheet = true }
+                .confirmationDialog("Title", isPresented: $showSheet) {
+                    Button("Action") {}
+                }
+                .confirmationDialogContent(isPresented: showSheet) {
+                    VStack {
+                        Text("Custom sheet content")
+                        Image(systemName: "hand.thumbsup.fill")
+                    }
+                    .padding()
+                }
+        }
+        .padding()
+    }
+}
+
 #Preview {
-    Practice()
+    AlertView()
 }
